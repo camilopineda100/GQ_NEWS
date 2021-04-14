b89ce97b83c37a77ea5aa58a5732fafb128ccdd6
Sebastian Pineda <camilopineda100@gmail.com>
2021-04-14 15:43:34 -0500

    feat: connect to db

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
+const mongoose = require('mongoose')
 
 // graphQL
 const typeDefs = require('./graphql/schema')
@@ -16,6 +17,16 @@ const server = new ApolloServer({
 server.applyMiddleware({ app })
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-    console.log(`Sever started on port ${PORT}`)
+mongoose.connect(`mongodb+srv://graphqluser:testing123@cluster0.mvizr.mongodb.net/GQ_NEWS?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Sever started on port ${PORT}`)
+    })
+}).catch((err) => {
+    console.log(err)
 })
+
