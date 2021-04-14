3b638db270466f297020290e5623dc90a7acf3a8
Sebastian Pineda <camilopineda100@gmail.com>
2021-04-14 16:25:37 -0500

    feat: user model

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'invalid email']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    name: {
+        type: String,
+        maxlength: 100
+    },
+    lastname: {
+        type: String,
+        maxlength: 100
+    },
+    token: {
+        type: String,
+    }
+})
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = { User }
