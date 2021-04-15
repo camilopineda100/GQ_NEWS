b4691057caebe17ff3921ceee5d525437ba51545
Sebastian Pineda <camilopineda100@gmail.com>
2021-04-15 08:39:04 -0500

    feat: query user

--- server/graphql/resolvers/query.js
+++ server/graphql/resolvers/query.js
@@ -1,7 +1,14 @@
+const { User } = require("../../models/user")
+
 module.exports = {
     Query: {
-        hello: async (parent, args, context, info) => {
-            return "Hello back"
+        user: async (parent, args, context, info) => {
+            try {
+                const user = await User.findOne({ _id: args.id })
+                return user
+            } catch(err) {
+                throw err
+            }
         }
     }
 }
