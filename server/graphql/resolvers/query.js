9b8ac460dae112e61068e21a237ce74688c53d9b
Sebastian Pineda <camilopineda100@gmail.com>
2021-04-15 11:42:53 -0500

    feat: isAuth query

--- server/graphql/resolvers/query.js
+++ server/graphql/resolvers/query.js
@@ -12,8 +12,24 @@ module.exports = {
                 if(req._id.toString() !== user._id.toString()) {
                     throw new AuthenticationError("You don't own this user")
                 }
-                
+
                 return user
+            } catch(err) {
+                throw err
+            }
+        },
+        isAuth: async (parent, args, context, info) => {
+            try {
+                const req = authorize(context.req, true)
+                if(!req._id) {
+                    throw new AuthenticationError("Bad token")
+                }
+                
+                return { 
+                    _id: req._id,
+                    email: req.email,
+                    token: req.token
+                }
             } catch(err) {
                 throw err
             }
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,17 +2,21 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const { AuthenticationError } = require('apollo-server-express')
 
-const authorize = (req) => {
+const throwAuthError = () => {
+    throw new AuthenticationError('Your are not auth')
+}
+
+const authorize = (req, verify = false) => {
     const authHeader = req.headers.authorization
     if(!authHeader) { 
         req.isAuth = false
-        throw new AuthenticationError('Your are not auth')
+        return !verify ? throwAuthError() : req
     }
 
     const token = authHeader.replace("Bearer ", "")
     if(!token || token === "") {
         req.isAuth = false
-        throw new AuthenticationError('Your are not auth')
+        return !verify ? throwAuthError() : req
     }
 
     let decodedJWT
@@ -20,18 +24,18 @@ const authorize = (req) => {
         decodedJWT = jwt.verify(token, process.env.SECRET)
         if(!decodedJWT) {
             req.isAuth = false
-            throw new AuthenticationError('Wrong token')
+            return !verify ? throwAuthError() : req
         }
 
         req.isAuth = true
         req._id = decodedJWT._id
         req.email = decodedJWT.email
+        req.token = token
+        return req
     } catch(err) {
         req.isAuth = false
-        throw new AuthenticationError('Error when authenticating')
+        return !verify ? throwAuthError() : req
     }
-
-    return req
 }
 
 module.exports = authorize
