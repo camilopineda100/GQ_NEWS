0930f3683fceba9b8067fa603213d9f2cd1ee9db
Sebastian Pineda <camilopineda100@gmail.com>
2021-04-23 15:31:13 -0500

    fix: update state when login

--- client/src/store/reducers/user.js
+++ client/src/store/reducers/user.js
@@ -2,6 +2,8 @@ export default function(state = {}, action) {
     switch(action.type) {
         case 'AUTH_USER': 
             return { ...state, ...action.payload}
+        case 'LOGIN_USER': 
+            return { ...state, ...action.payload}
         default:
             return state
     }
